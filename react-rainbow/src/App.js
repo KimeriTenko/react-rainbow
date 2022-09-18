c06699657f7aab9326f9262aeecfefb484e01b3c
Mary Frye <deimospho@gmail.com>
2022-09-18 00:57:30 -0400

    ADDED colorForm, EDIT index.js, app.js, package.json

--- react-rainbow/src/App.js
+++ react-rainbow/src/App.js
@@ -1,29 +1,41 @@
+import './App.css';
 import React, { useState } from 'react'
 import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
+
+
 
 function App(){
-    let colors = [
-      'violet', 
-      'blue', 
-      'lightblue', 
-      'green', 
-      'greenyellow', 
-      'yellow', 
-      'orange', 
-      'red'
-    ]
-
+    let [colors, setColors] = useState([
+        'violet', 
+        'blue',
+        'lightblue', 
+        'green',
+        'greenyellow', 
+        'yellow',
+        'orange', 
+        'red'
+    ])
+    
     let colorMap = colors.map((color, i) => {
-      return (
-        <ColorBlock color={color} />
-      )
+        return (
+            <ColorBlock key={color} />
+        )
     })
+
+    const addColor = (newColor) => {
+        setColors([...colors, newColor])
+    }
 
     return (
         <div className="App">
-          {colorMap}
+            {colorMap}
+            <ColorForm addColor={addColor} />
         </div>
     )
+
 }
+
+
 
 export default App;
